650a3b1223d07f25cc9428cef976bef49460e067
swaroop190182 <swaroop.alwar@gmail.com>
2026-06-02 12:10:58 +0530

    Admin entry point — mounts Admin directly

---
+++ src/admin-main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Admin from './views/Admin'
+
+ReactDOM.createRoot(document.getElementById('admin-root')!).render(
+  <React.StrictMode>
+    <Admin />
+  </React.StrictMode>
+)
