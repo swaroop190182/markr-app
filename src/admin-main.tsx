cd5d8256387cce9eee7147a1337ffc7121b3678e
swaroop190182 <swaroop.alwar@gmail.com>
2026-06-03 12:14:41 +0530

    Remove admin-main.tsx — causing build issues

--- src/admin-main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Admin from './views/Admin'
-
-ReactDOM.createRoot(document.getElementById('admin-root')!).render(
-  <React.StrictMode>
-    <Admin />
-  </React.StrictMode>
-)
